b44c8c43c0bed464fe8f9e67a3796840daa695d0
this-is-this-is <dlrjs529@gmail.com>
2024-07-15 13:08:28 +0900

    #11660 구간 합 구하기2

--- solved.py
+++ solved.py
@@ -2,15 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n,q = map(int, input().split())
-arr = list(map(int, input().split()))
+arrA = [[0]*(n+1)]
+arrS = [[0]*(n+1) for _ in range(n+1)]
 
-sumarr = [0]
-sum = 0
+for i in range(n):
+    arrTemp = [0] + list(map(int, input().split()))
+    arrA.append(arrTemp)
 
-for i in arr:
-    sum += i    
-    sumarr.append(sum)
-   
+for i in range(1, n+1):
+    for j in range(1, n+1):
+        arrS[i][j] = arrS[i-1][j] + arrS[i][j-1] - arrS[i-1][j-1] + arrA[i][j]
+
 for i in range(q):
-    s,e = map(int, input().split())
-    print(sumarr[e] - sumarr[s-1])
+    x1, y1, x2, y2 = map(int, input().split())
+    result = arrS[x2][y2] - arrS[x2][y1-1] - arrS[x1-1][y2] + arrS[x1-1][y1-1]
+    print(result)
+
+
