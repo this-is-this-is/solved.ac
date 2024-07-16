21486f7bd9b791bb6e4dbb9fcf5c38c904145fac
this-is-this-is <dlrjs529@gmail.com>
2024-07-16 22:07:52 +0900

    #12891 S2 DNA 비밀번호

--- solved.py
+++ solved.py
@@ -1,27 +1,58 @@
 import sys
 input = sys.stdin.readline
 
-n = int(input())
-arr = list(map(int, input().split()))
+def compare():
+    global staticlist, prelist, count
+    if(staticlist[0] <= prelist[0] and staticlist[1] <= prelist[1] and staticlist[2] <= prelist[2] and staticlist[3] <= prelist[3]):
+        count+= 1
 
-arr.sort()
+def removeChar(c):
+    global prelist
+    if(c == 'A'):
+        prelist[0] -= 1
+    elif(c == 'C'):
+        prelist[1] -= 1    
+    elif(c == 'G'):
+        prelist[2] -= 1  
+    elif(c == 'T'):
+        prelist[3] -= 1
+
+def addChar(c):
+    global prelist
+    if(c == 'A'):
+        prelist[0] += 1
+    elif(c == 'C'):
+        prelist[1] += 1    
+    elif(c == 'G'):
+        prelist[2] += 1  
+    elif(c == 'T'):
+        prelist[3] += 1  
+# A C G T
+s, p = map(int, input().split())
+arr = list(input())
+
+staticlist = list(map(int, input().split()))
+prelist = [0]*4
 count = 0
-for i in range(n):
-    s = 0
-    e = n-1
-    value = arr[i]
-    while(s < e):
-        if(arr[s]+ arr[e] < value):
-            s += 1
-        elif(arr[s] + arr[e] == value):
-            if(s != i and e != i):
-                e -= 1
-                count += 1
-                break
-            elif(s == i):
-                s+=1
-            elif(e == i):
-                e-=1
-        else:
-            e -= 1
+
+for i in range(p):
+    if(arr[i] == 'A'):
+        prelist[0] += 1
+    elif(arr[i] == 'C'):
+        prelist[1] +=1
+    elif(arr[i] == 'G'):
+        prelist[2] +=1
+    elif(arr[i] == 'T'):
+        prelist[3] +=1
+
+start = 0
+end = p -1
+
+while(end < s):
+    compare()
+    addChar(arr[end + 1])
+    removeChar(arr[start])
+    start += 1
+    end += 1
+
 print(count)
