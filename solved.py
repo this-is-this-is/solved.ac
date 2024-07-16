65f42bdf8dc9da17f15700dd5ff2f0eefe3943cd
this-is-this-is <dlrjs529@gmail.com>
2024-07-16 11:00:01 +0900

    #1940 S4 주몽

--- solved.py
+++ solved.py
@@ -2,21 +2,20 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-count = 1
-s = 1
-e = 1
-sum = 1
-while e < n:
-    if(sum < n):
-        e += 1
-        sum += e
-    elif(sum == n):
-        e += 1
-        sum += e
-        count += 1
-    else:
-        sum -= s
-        s += 1
-        
+m = int(input())
+arr = list(map(int, input().split()))
 
+arr.sort()
+
+count = 0
+s = 0
+e = n-1
+while(s < e):
+    if(arr[s] + arr[e] > m):
+        e-= 1
+    elif(arr[s] + arr[e] == m):
+        e -= 1
+        count+=1
+    else:
+        s +=1
 print(count)
