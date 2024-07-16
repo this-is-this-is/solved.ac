bbe838845df609dfa4305ff81ff6d51e36f39204
this-is-this-is <dlrjs529@gmail.com>
2024-07-16 11:14:29 +0900

    #1253 G4 좋은 수

--- solved.py
+++ solved.py
@@ -2,20 +2,26 @@ import sys
 input = sys.stdin.readline
 
 n = int(input())
-m = int(input())
 arr = list(map(int, input().split()))
 
 arr.sort()
-
 count = 0
-s = 0
-e = n-1
-while(s < e):
-    if(arr[s] + arr[e] > m):
-        e-= 1
-    elif(arr[s] + arr[e] == m):
-        e -= 1
-        count+=1
-    else:
-        s +=1
+for i in range(n):
+    s = 0
+    e = n-1
+    value = arr[i]
+    while(s < e):
+        if(arr[s]+ arr[e] < value):
+            s += 1
+        elif(arr[s] + arr[e] == value):
+            if(s != i and e != i):
+                e -= 1
+                count += 1
+                break
+            elif(s == i):
+                s+=1
+            elif(e == i):
+                e-=1
+        else:
+            e -= 1
 print(count)
