7820cc7180309cb2f994c12a8aee55cd26d9bb43
this-is-this-is <dlrjs529@gmail.com>
2024-07-20 22:34:38 +0900

    #15663 S2 N과 M (9)

--- solved.py
+++ solved.py
@@ -1,17 +1,31 @@
 import sys
 input = sys.stdin.readline
 
-def go(a, b, c):
-    if b == 1:
-        return a % c
-    ret = go(a, b // 2, c)
-    ret = (ret * ret) % c
-    if b % 2:
-        ret = (ret * a) % c
-    return ret
+n, m = map(int, input().split())
+arr = list(map(int, input().split()))
+arr.sort()
+stack = []
+def isOverlap(index):
+    global arr, stack
+    if stack.count(arr[index]) > arr.count(arr[index]):
+        return True
+    return False
 
-a, b, c = map(int, input().split())
-
-print(go(a, b, c))
-
-
+def backTracking(start):
+    if len(stack) == m:
+          for i in stack:
+              print(i, end = " ")
+          print("")
+          return
+    
+    for i in range(start, n):
+      if i < n - 1 and arr[i] == arr[i+1]:
+          continue
+      stack.append(arr[i])
+      if isOverlap(i):
+          stack.pop()
+          continue
+      backTracking(0) 
+      stack.pop()
+  
+backTracking(0)
