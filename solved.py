d27ecf26ca9a8a9490dfc114b16d9f1549e121a8
this-is-this-is <dlrjs529@gmail.com>
2024-07-20 20:00:27 +0900

    #1213 S3 팰린드롬 만들기

--- solved.py
+++ solved.py
@@ -1,21 +1,28 @@
 import sys
 input = sys.stdin.readline
-n, m = map(int, input().split())
-arr = list(map(int, input().split()))
+from collections import deque
+arr = list(input())
 arr.sort()
-result = []
-
-def backTracking(start):
-  if len(result) == m:
-    for i in range(m):
-      print(result[i], end=" ")
-    print("")
-    return
-
-  for i in range(start,n):
-    if arr[i] not in result:
-        result.append(arr[i])
-        backTracking(0)
-        result.pop()
-
-backTracking(0)
+arr.reverse()
+mydeque = deque()
+index = 0
+tmp = []
+while index < len(arr) - 1:
+    if index == len(arr) - 2:
+        tmp.append(arr[index])
+        index+=1
+    elif arr[index] == arr[index + 1]:
+        mydeque.appendleft(arr[index])
+        mydeque.append(arr[index + 1])
+        index += 2
+    else :
+        tmp.append(arr[index])
+        index+=1
+if len(tmp) > 1:
+    print("I'm Sorry Hansoo")
+else :
+    if tmp:
+      mydeque.insert(len(mydeque)//2, tmp[0])
+    for i in mydeque :
+      print(i, end = "")
+    
