1a878330c6c238f9c6ff084b3fa39036fda68a75
this-is-this-is <dlrjs529@gmail.com>
2024-07-21 18:32:18 +0900

    #15666 S2 N과 M (12)

--- solved.py
+++ solved.py
@@ -4,28 +4,21 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 arr = list(map(int, input().split()))
 arr.sort()
-stack = []
-def isOverlap(index):
-    global arr, stack
-    if stack.count(arr[index]) > arr.count(arr[index]):
-        return True
-    return False
+stack = []  
 
 def backTracking(start):
-    if len(stack) == m:
-          for i in stack:
-              print(i, end = " ")
-          print("")
-          return
-    
+    if len(stack) == m :
+        for i in stack:
+            print(i, end = " ")
+        print("")
+        return
+
     for i in range(start, n):
-      if i < n - 1 and arr[i] == arr[i+1]:
-          continue
-      stack.append(arr[i])
-      if isOverlap(i):
-          stack.pop()
-          continue
-      backTracking(0) 
-      stack.pop()
-  
+        if i < n - 1 and arr[i] == arr[i+1] :
+            continue
+
+
+        stack.append(arr[i])
+        backTracking(i)
+        stack.pop()
 backTracking(0)
