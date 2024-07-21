fe57d8e784420bd46dca6893fef615071e7bacf1
this-is-this-is <dlrjs529@gmail.com>
2024-07-21 18:49:50 +0900

    #11053 S2 가장 긴 증가하는 부분 수열

--- solved.py
+++ solved.py
@@ -1,24 +1,13 @@
 import sys
 input = sys.stdin.readline
 
-n, m = map(int, input().split())
+n = int(input())
 arr = list(map(int, input().split()))
-arr.sort()
-stack = []  
+dp = [1]*n
 
-def backTracking(start):
-    if len(stack) == m :
-        for i in stack:
-            print(i, end = " ")
-        print("")
-        return
+for i in range(1, n):
+    for j in range(i):
+        if arr[i] > arr[j]: 
+            dp[i] = max(dp[i], dp[j] + 1)  
 
-    for i in range(start, n):
-        if i < n - 1 and arr[i] == arr[i+1] :
-            continue
-
-
-        stack.append(arr[i])
-        backTracking(i)
-        stack.pop()
-backTracking(0)
+print(max(dp))
