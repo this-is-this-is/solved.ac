8effd303427c4553388f052dda09abdae557a44d
this-is-this-is <dlrjs529@gmail.com>
2024-07-22 11:26:12 +0900

    #1517 P5 버블 소트

--- solved.py
+++ solved.py
@@ -1,24 +1,45 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-def BFS(A, B):
-    queue = deque([(A, 1)])  # (현재 숫자, 연산 횟수)
+def merge_sort(s, e):
+    if e-s < 1:
+        return
+    global count
+    m = s + (e - s)//2
+    merge_sort(s, m)
+    merge_sort(m+1, e)
+
+    for i in range(s, e + 1):
+        tmp[i] = arr[i]
     
-    while queue:
-        current, operations = queue.popleft()
-        
-        if current == B:
-            return operations
-        
-        if current * 2 <= B:
-            queue.append((current * 2, operations + 1))
-        
-        if int(str(current) + '1') <= B:
-            queue.append((int(str(current) + '1'), operations + 1))
+    k = s
+    index1 = s
+    index2 = m + 1
     
-    return -1
+    while index1 <= m and index2 <= e:
+        if tmp[index1] <= tmp[index2]:
+            arr[k] = tmp[index1]
+            k+=1
+            index1+=1
+        else:
+            arr[k] = tmp[index2]
+            count += (index2 - k) 
+            k+=1
+            index2+=1
 
-A, B = map(int, input().split())
+    while index1 <= m:
+        arr[k] = tmp[index1]
+        k+=1
+        index1+=1
 
-print(BFS(A, B))
+    while index2 <= e:
+        arr[k] = tmp[index2]
+        k+=1
+        index2+=1        
+
+n = int(input())
+arr = list(map(int, input().split()))
+tmp = [0]*n
+count = 0
+merge_sort(0,n-1)
+print(count)
