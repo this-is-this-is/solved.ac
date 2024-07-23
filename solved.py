f147202db7d307d9c9d2481f017f2b6c4d41cbd8
this-is-this-is <dlrjs529@gmail.com>
2024-07-23 10:54:50 +0900

    #13023 G5 ABCDE

--- solved.py
+++ solved.py
@@ -2,25 +2,32 @@ import sys
 input = sys.stdin.readline
 sys.setrecursionlimit(10000)
 
-n = int(input())
+n,m = map(int, input().split())
+arr = [[] for _ in range(n+1)]
+visited = [False] * (n+1)
+arrive = False
 
-def isPrime(value):
-    for i in range(2, int(value/2 + 1)):
-        if value % i == 0:
-            return False
-    return True
+def DFS(v, depth):
+    global arrive
+    if depth == 5:
+        arrive = True
+        return
+    visited[v] = True
+    for i in arr[v]:
+        if not visited[i]:
+            DFS(i, depth + 1)
+    visited[v] = False
+for _ in range(m):
+    v1, v2 = map(int,input().split())
+    arr[v1].append(v2)
+    arr[v2].append(v1)
 
-def DFS(value):
-    if len(str(value)) == n:
-        print(value)
-    else:
-        for i in range(10):
-            if i % 2 == 0:
-                continue
-            if isPrime(value*10 + i):
-                DFS(value*10 + i)
-            
-DFS(2)
-DFS(3)
-DFS(5)
-DFS(7)
+for i in range(n):
+    DFS(i, 1)
+    if arrive:
+        break
+
+if arrive :
+    print(1)
+else:
+    print(0)
