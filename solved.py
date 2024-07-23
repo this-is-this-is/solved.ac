3fe801a7e3fdaf4447d1787f8670f598312e3922
this-is-this-is <dlrjs529@gmail.com>
2024-07-23 12:33:25 +0900

    #1463 S3 1로 만들기 / BFS

--- solved.py
+++ solved.py
@@ -1,13 +1,30 @@
 import sys
 input = sys.stdin.readline
+from collections import deque
+
+def BFS(n):
+    queue = deque([(n, 0)])
+    
+    visited = set()
+    visited.add(n)
+    
+    while queue:
+        current, operations = queue.popleft()
+        
+        if current == 1:
+            return operations
+        
+        if current - 1 not in visited:
+            visited.add(current - 1)
+            queue.append((current - 1, operations + 1))
+        
+        if current % 2 == 0 and current // 2 not in visited:
+            visited.add(current // 2)
+            queue.append((current // 2, operations + 1))
+        
+        if current % 3 == 0 and current // 3 not in visited:
+            visited.add(current // 3)
+            queue.append((current // 3, operations + 1))
 
 n = int(input())
-arr = list(map(int, input().split()))
-arr.sort()
-
-sarr = [0]
-tmp = 0
-for i in arr:
-    tmp += i
-    sarr.append(tmp)
-print(sum(sarr))
+print(BFS(n))
