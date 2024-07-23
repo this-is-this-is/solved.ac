f41e88c0976f2f3997149387052252caa680e4ac
this-is-this-is <dlrjs529@gmail.com>
2024-07-23 11:39:30 +0900

    #10026 G5 적록색약

--- solved.py
+++ solved.py
@@ -1,33 +1,49 @@
 import sys
+import copy
 input = sys.stdin.readline
 sys.setrecursionlimit(10000)
 
-n,m = map(int, input().split())
-arr = [[] for _ in range(n+1)]
-visited = [False] * (n+1)
-arrive = False
-
-def DFS(v, depth):
-    global arrive
-    if depth == 5:
-        arrive = True
-        return
-    visited[v] = True
-    for i in arr[v]:
-        if not visited[i]:
-            DFS(i, depth + 1)
-    visited[v] = False
-for _ in range(m):
-    v1, v2 = map(int,input().split())
-    arr[v1].append(v2)
-    arr[v2].append(v1)
+n = int(input())
+visited1 = [[0 for _ in range(n)] for _ in range(n)]
+visited2 = [[0 for _ in range(n)] for _ in range(n)]
+normal = []
+for i in range(n):
+    normal.append(list(input()))
+unnormal = copy.deepcopy(normal)
 
 for i in range(n):
-    DFS(i, 1)
-    if arrive:
-        break
+    for j in range(n):
+        if unnormal[i][j] == 'G':
+            unnormal[i][j] = 'R'
 
-if arrive :
-    print(1)
-else:
-    print(0)
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+cnt = 0
+count1 = 0
+count2 = 0
+
+def DFS(x, y, arr, visited):
+    global cnt
+    if visited[x][y] == 1:
+        return
+    cnt += 1
+    visited[x][y] = 1
+    for i in range(4):
+        if 0 <= x+dx[i] <= n-1 and 0 <= y+dy[i] <= n-1:
+            if arr[x][y] == arr[x + dx[i]][y + dy[i]] and visited[x + dx[i]][y + dy[i]] == 0:
+                DFS(x + dx[i], y + dy[i], arr, visited)
+for i in range(n):
+    for j in range(n):
+        if visited1[i][j] == 0:
+            DFS(i, j, normal, visited1)
+        if cnt != 0:
+            count1+=1
+        cnt = 0
+
+        if visited2[i][j] == 0:
+            DFS(i, j, unnormal, visited2)
+        if cnt != 0:
+            count2+=1
+        cnt = 0
+
+print(count1, count2)
