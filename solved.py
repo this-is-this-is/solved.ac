9e21d800c5886ff52fd64e70a6a7508fb759c369
this-is-this-is <dlrjs529@gmail.com>
2024-07-24 10:24:13 +0900

    #1654 S2 랜선 자르기 / 이분 탐색

--- solved.py
+++ solved.py
@@ -1,30 +1,31 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
 
-def BFS(n):
-    queue = deque([(n, 0)])
-    
-    visited = set()
-    visited.add(n)
-    
-    while queue:
-        current, operations = queue.popleft()
-        
-        if current == 1:
-            return operations
-        
-        if current - 1 not in visited:
-            visited.add(current - 1)
-            queue.append((current - 1, operations + 1))
-        
-        if current % 2 == 0 and current // 2 not in visited:
-            visited.add(current // 2)
-            queue.append((current // 2, operations + 1))
-        
-        if current % 3 == 0 and current // 3 not in visited:
-            visited.add(current // 3)
-            queue.append((current // 3, operations + 1))
+count = 0
+max_len = 0
+k, n = map(int, input().split())
+arr = []
 
-n = int(input())
-print(BFS(n))
+for i in range(k):
+    arr.append(int(input()))
+    
+start = 1
+end = max(arr)   
+
+while (start <= end):
+    mid = (start+end)//2
+
+    for i in arr:
+        count += i//mid
+    
+    if count >= n:
+        max_len = mid
+        start = mid + 1
+    elif count < n:
+        end = mid - 1
+
+    count = 0
+    
+print(max_len)
+    
+
