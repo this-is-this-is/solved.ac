676806a0ab1f2ece709018ba48c17144b7d54dda
this-is-this-is <dlrjs529@gmail.com>
2024-07-26 10:56:46 +0900

    #2343 S1 기타 레슨 / 이분 탐색

--- solved.py
+++ solved.py
@@ -2,37 +2,28 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n = int(input())
-arr = [[] for _ in range(n+1)]
+n,m = map(int,input().split())
+arr = list(map(int, input().split()))
 
-for i in range(n):
-    tmp = list(map(int, input().split()))
-    j = 1
-    while True:
-        if tmp[j] == -1:
-            break
-        arr[tmp[0]].append((tmp[j],tmp[j+1]))
-        j+=2
+start = max(arr)
+end = sum(arr)
 
-def BFS(start):
-    queue = deque()
-    queue.append(start)
-    visited[start] = True
-    while queue:
-        now = queue.popleft()
-        for i in arr[now]:
-            if not visited[i[0]]:
-                visited[i[0]] = True
-                queue.append(i[0])
-                distance[i[0]] = distance[now] + i[1]
-            
-visited = [False] *(n+1)
-distance = [0] * (n+1)       
-BFS(1)
+while start <= end:
+    mid = (start + end) // 2
+    count = 0
+    sum = 0
+    for i in arr:
+        if sum + i > mid:
+            count+=1
+            sum = 0
+        sum+= i
 
-max_index = distance.index(max(distance))
-visited = [False] *(n+1)
-distance = [0] * (n+1)
-BFS(max_index)
+    if sum != 0 :
+        count+=1
+        
+    if count > m:
+        start = mid + 1
+    else:
+        end = mid - 1
 
-print(max(distance))
+print(start)
