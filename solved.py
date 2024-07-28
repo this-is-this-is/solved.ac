f590ee8c42f42370f5d1a9eb36782edbf89cd59f
this-is-this-is <dlrjs529@gmail.com>
2024-07-28 18:28:57 +0900

    #1389 S1 케빈 베이컨의 6단계 법칙 / BFS

--- solved.py
+++ solved.py
@@ -2,20 +2,32 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n = int(input())
-k = int(input())
+n, m = map(int, input().split())
+visited = [False] *(n+1)
+result = [0]*(n)
+arr = [[]for _ in range(n+1)]
+queue = deque()
 
-start = 1
-end = k
-result = 0
-while start <= end:
-    mid = (start + end) //2
-    count = 0
-    for i in range(1, n+1):
-        count += min(mid//i,n)
-    if count < k:
-        start = mid + 1
-    else:
-        result = mid
-        end = mid -1
-print(result)
+for i in range(m):
+    s, e = map(int, input().split())
+    arr[s].append(e)
+    arr[e].append(s)
+
+def BFS(start):
+    visited[start] = True
+    queue.append(start)
+    sumarr = [0]*(n+1)
+    depth = 1
+    while queue:
+        now = queue.popleft()
+        visited[now] = True
+        for i in arr[now]:
+            if not visited[i]:
+                visited[i] = True
+                queue.append(i)
+                sumarr[i] = sumarr[now] +1
+    result[start-1] = sum(sumarr) 
+for i in range(1, n+1):
+    visited = [False] *(n+1)
+    BFS(i)
+print(result.index(min(result))+1)
