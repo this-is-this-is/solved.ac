cefbe12624cb5c9fe9681de6a6e65c6dc3ac749d
this-is-this-is <dlrjs529@gmail.com>
2024-07-28 19:20:53 +0900

    #11725 S2 트리의 부모 찾기 / DFS

--- solved.py
+++ solved.py
@@ -1,33 +1,24 @@
 import sys
+sys.setrecursionlimit(10000000)
 input = sys.stdin.readline
 from collections import deque
 
-n, m = map(int, input().split())
-visited = [False] *(n+1)
-result = [0]*(n)
-arr = [[]for _ in range(n+1)]
-queue = deque()
-
-for i in range(m):
-    s, e = map(int, input().split())
+n=int(input())
+visited=[False]*(n+1)
+arr = [[] for _ in range(n+1)]
+result=[0]*(n+1)
+for i in range(n-1):
+    s,e=map(int,input().split())
     arr[s].append(e)
     arr[e].append(s)
 
-def BFS(start):
-    visited[start] = True
-    queue.append(start)
-    sumarr = [0]*(n+1)
-    depth = 1
-    while queue:
-        now = queue.popleft()
-        visited[now] = True
-        for i in arr[now]:
-            if not visited[i]:
-                visited[i] = True
-                queue.append(i)
-                sumarr[i] = sumarr[now] +1
-    result[start-1] = sum(sumarr) 
-for i in range(1, n+1):
-    visited = [False] *(n+1)
-    BFS(i)
-print(result.index(min(result))+1)
+def DFS(v):
+    visited[v]=True
+    for i in arr[v]:
+        if not visited[i]:
+            visited[i] = True
+            result[i]=v
+            DFS(i)
+DFS(1)
+for i in range(2,n+1):
+    print(result[i])
