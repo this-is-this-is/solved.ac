1978bc3eac8714ef6dd79e95a2f848d3d2e8deef
this-is-this-is <dlrjs529@gmail.com>
2024-07-29 21:03:48 +0900

    #17218 G4 비밀번호 만들기 / LCS

--- solved.py
+++ solved.py
@@ -1,24 +1,32 @@
 import sys
-sys.setrecursionlimit(10000000)
 input = sys.stdin.readline
-from collections import deque
+from heapq import heappush, heappop
 
-n=int(input())
-visited=[False]*(n+1)
-arr = [[] for _ in range(n+1)]
-result=[0]*(n+1)
-for i in range(n-1):
-    s,e=map(int,input().split())
-    arr[s].append(e)
-    arr[e].append(s)
-
-def DFS(v):
-    visited[v]=True
-    for i in arr[v]:
-        if not visited[i]:
-            visited[i] = True
-            result[i]=v
-            DFS(i)
-DFS(1)
-for i in range(2,n+1):
-    print(result[i])
+arr1 = list(input())
+arr1.remove('\n')
+arr2 = list(input())
+arr2.remove('\n')
+n = len(arr1) + 1
+m = len(arr2) + 1
+LCS = [[0 for _ in range(m)] for _ in range(n)]
+for i in range(n - 1):
+    for j in range(m - 1):
+        if arr1[i] != arr2[j]:  
+            LCS[1+i][1+j] = max(LCS[i][1+j], LCS[1+i][j])
+        else:
+            LCS[1+i][1+j] = LCS[i][j] + 1
+result = []
+x = m-1
+y = n-1
+while x > 0 and y > 0:
+    if LCS[y][x] == LCS[y-1][x]:
+        y -= 1
+    elif LCS[y][x] == LCS[y][x-1]:
+        x -= 1
+    else:
+        y -= 1
+        x -= 1
+        result.append(arr1[y])
+result.reverse()
+for i in result:
+    print(i, end = '')
