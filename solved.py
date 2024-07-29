a16f842b917a74089ad89087e246ef09c27b40f8
this-is-this-is <dlrjs529@gmail.com>
2024-07-29 21:43:52 +0900

    #1012 S2 유기농 배추 / DFS

--- solved.py
+++ solved.py
@@ -1,32 +1,28 @@
 import sys
 input = sys.stdin.readline
-from heapq import heappush, heappop
+sys.setrecursionlimit(10000000)
 
-arr1 = list(input())
-arr1.remove('\n')
-arr2 = list(input())
-arr2.remove('\n')
-n = len(arr1) + 1
-m = len(arr2) + 1
-LCS = [[0 for _ in range(m)] for _ in range(n)]
-for i in range(n - 1):
-    for j in range(m - 1):
-        if arr1[i] != arr2[j]:  
-            LCS[1+i][1+j] = max(LCS[i][1+j], LCS[1+i][j])
-        else:
-            LCS[1+i][1+j] = LCS[i][j] + 1
-result = []
-x = m-1
-y = n-1
-while x > 0 and y > 0:
-    if LCS[y][x] == LCS[y-1][x]:
-        y -= 1
-    elif LCS[y][x] == LCS[y][x-1]:
-        x -= 1
-    else:
-        y -= 1
-        x -= 1
-        result.append(arr1[y])
-result.reverse()
-for i in result:
-    print(i, end = '')
+t = int(input())
+def DFS(y,x):
+    visited[y][x] = True
+    dx = [0,0,-1,1]
+    dy = [-1,1,0,0]
+    for i in range(4):
+        if y+dy[i] <= n  and x+dx[i] <= m and y+dy[i] >= 0  and x+dx[i] >= 0:
+            if visited[y+dy[i]][x+dx[i]] == False and arr[y+dy[i]][x+dx[i]] == 1:
+                DFS(y+dy[i],x+dx[i])
+
+for i in range(t):
+    m, n, k= map(int,input().split())
+    count = 0
+    visited = [[False for _ in range(m+1)]for _ in range(n+1) ] 
+    arr = [[0 for _ in range(m+1)]for _ in range(n+1) ] 
+    for i in range(k):
+        x, y = map(int,input().split())
+        arr[y+1][x+1] = 1
+    for i in range(1,n+1):
+        for j in range(1,m+1):
+            if visited[i][j] == False and arr[i][j] == 1:
+                DFS(i,j)
+                count+=1
+    print(count)
