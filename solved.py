c6c9ee36b855f6f538c7a70adeecf69384aceb22
this-is-this-is <dlrjs529@gmail.com>
2024-08-02 15:23:01 +0900

    #1931 S1 회의실 배정 / Greedy

--- solved.py
+++ solved.py
@@ -2,35 +2,20 @@ import sys
 input = sys.stdin.readline
 from queue import PriorityQueue
 
-n = int(sys.stdin.readline().strip())
-maxq = PriorityQueue()
-minq = PriorityQueue()
-
-one = 0
+n = int(input())
+queue = PriorityQueue()
 for i in range(n):
+    s, e = map(int, input().split())
+    queue.put((e,s))
 
-    x = int(input())
-    if x == 1 :
-        one = 1
-    if x > 0:
-        maxq.put(-x)
-    elif x <= 0:
-        minq.put(x)
-
-result = []
-while maxq.qsize() >= 2:
-    a , b = -maxq.get(),-maxq.get()
-    if a != 1 and b != 1:
-      result.append(a*b)
+count = 1
+arr = [queue.get()]
+while not queue.empty():
+    value = queue.get()
+    if arr[-1][0] > value[1]:
+        continue
     else:
-        result.append(a)
-        result.append(b)
-if maxq.qsize() == 1:
-    result.append(-maxq.get())
-      
-while minq.qsize() >= 2:
-    result.append(minq.get()*minq.get())
-if minq.qsize() == 1:
-    result.append(minq.get())
-
-print(sum(result))
+        arr.append(value)
+        count+=1
+print(count)
+    
