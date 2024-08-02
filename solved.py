5b4b22e7202f3d1aa6fd3c7df4b3fd87f0460212
this-is-this-is <dlrjs529@gmail.com>
2024-08-02 16:14:31 +0900

    #1456 G5 거의 소수 / 정수론

--- solved.py
+++ solved.py
@@ -1,21 +1,31 @@
 import sys
 input = sys.stdin.readline
-from queue import PriorityQueue
+import math
+s, e = map(int, input().split())
 
-n = int(input())
-queue = PriorityQueue()
-for i in range(n):
-    s, e = map(int, input().split())
-    queue.put((e,s))
+count= 0
+A = [0]*10000001
 
-count = 1
-arr = [queue.get()]
-while not queue.empty():
-    value = queue.get()
-    if arr[-1][0] > value[1]:
+for i in range(2, len(A)):
+    A[i] = i
+
+for i in range(2,int(math.sqrt(len(A)))+1):
+    if A[i] == 0:
         continue
     else:
-        arr.append(value)
-        count+=1
+        for j in range(i+i, len(A), i):
+            A[j] = 0
+for i in range(2, len(A)):
+    if A[i] == 0:
+        continue
+    tmp = A[i]
+    while A[i] <= e/tmp:
+        if A[i] >= s/tmp:
+            count+=1
+        tmp *= A[i]
+        
 print(count)
+
+
+
     
