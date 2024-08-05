ad06c09f06a30bc0414467b1a2ba2cea9d4a9dc7
this-is-this-is <dlrjs529@gmail.com>
2024-08-05 20:17:07 +0900

    #2251 G4 물통 / 그래프

--- solved.py
+++ solved.py
@@ -1,38 +1,45 @@
 import sys
 input = sys.stdin.readline
-sys.setrecursionlimit(10**6)
 
-def DFS(v):
-    global isP
-    visited[v] = True
-    for i in arr[v]:
-        if not visited[i]:
-            check[i] = (check[v]+1)%2
-            DFS(i)
-        elif check[i] == check[v]:
-            isP = False
-k = int(input())
+from collections import deque
 
-for i in range(k):
-    v,e = map(int,input().split())
-    arr = [[] for _ in range(v+1)]
-    visited = [False]*(v+1)
-    check = [0]*(v+1)
-    isP = True
-    for j in range(e):
-        start, end = map(int,input().split())
-        arr[start].append(end)
-        arr[end].append(start)
-    
-    for l in range(1, v+1):
-        if isP:
-            DFS(l)
-        else:
-            break
+send = [0,0,1,1,2,2]
+receive = [1,2,0,2,0,1]
 
-    if isP:
-        print('YES')
-    else:
-        print('NO')
+abc = list(map(int, input().split()))
+visited = [[False for j in range(201)] for i in range(201)]
+result = [False] * 201
+
+def BFS():
+    queue = deque()
+    queue.append((0,0))
+    visited[0][0] = True
+    result[abc[2]] = True
+    while queue:
+        now = queue.popleft()
+        a = now[0]
+        b = now[1]
+        c = abc[2] - a - b
+        for i in range(6):
+            next = [a,b,c]
+            next[receive[i]] += next[send[i]]
+            next[send[i]] = 0
+
+            if next[receive[i]] > abc[receive[i]]:
+                next[send[i]] = next[receive[i]] - abc[receive[i]]
+                next[receive[i]] = abc[receive[i]]
+
+            if not visited[next[0]][next[1]]:
+                visited[next[0]][next[1]] = True
+                queue.append((next[0],next[1]))
+                if next[0] == 0:
+                    result[next[2]] = True
+BFS()
+
+for i in range(len(result)):
+    if result[i]:
+        print(i, end=' ')
+        
+
 
 
