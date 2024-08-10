80f9a138e90d404bca790f20729e3eed2b13d9b5
this-is-this-is <dlrjs529@gmail.com>
2024-08-10 17:10:13 +0900

    #1516 G3 게임 개발 / 그래프(위상정렬)

--- solved.py
+++ solved.py
@@ -2,14 +2,22 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n,m = map(int, input().split())
+n = int(input())
 arr = [[]for _ in range(n+1)]
 indegree = [0] *(n+1)
+selfBuild = [0]*(n+1)
 
-for i in range(m):
-    s,e = map(int, input().split())
-    arr[s].append(e)
-    indegree[e] += 1
+for i in range(1, n+1):
+    inputList = list(map(int, input().split()))
+    selfBuild[i] = (inputList[0])
+    index = 1
+    while True:
+        preTemp = inputList[index]
+        index+=1
+        if preTemp == -1:
+            break
+        arr[preTemp].append(i)
+        indegree[i]+=1
 
 queue = deque()
 
@@ -17,13 +25,18 @@ for i in range(1,n+1):
     if indegree[i] == 0:
         queue.append(i)
 
+result = [0]*(n+1)
+
 while queue:
     now = queue.popleft()
-    print(now, end = ' ')
-    for i in arr[now]:
-        indegree[i] -= 1
-        if indegree[i] == 0:
-            queue.append(i)
+    for next in arr[now]:
+        indegree[next] -= 1
+        result[next] = max(result[next], result[now]+selfBuild[now])
+        if indegree[next] == 0:
+            queue.append(next)
+
+for i in range(1,n+1):
+    print(result[i]+selfBuild[i])
 
 
 
