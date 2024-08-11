beb851c0287bd109e9a36fba0441a4e9dd977f95
this-is-this-is <dlrjs529@gmail.com>
2024-08-11 18:10:58 +0900

    #1948 P5 임계경로 / 그래프(위상정렬)

--- solved.py
+++ solved.py
@@ -3,40 +3,49 @@ input = sys.stdin.readline
 from collections import deque
 
 n = int(input())
+m = int(input())
 arr = [[]for _ in range(n+1)]
+reverse = [[]for _ in range(n+1)]
 indegree = [0] *(n+1)
-selfBuild = [0]*(n+1)
+result = [0]*(n+1)
+count = 0
+visited = [False]*(n+1)
 
-for i in range(1, n+1):
-    inputList = list(map(int, input().split()))
-    selfBuild[i] = (inputList[0])
-    index = 1
-    while True:
-        preTemp = inputList[index]
-        index+=1
-        if preTemp == -1:
-            break
-        arr[preTemp].append(i)
-        indegree[i]+=1
+for i in range(m):
+    s,e,v = map(int, input().split())
+    arr[s].append((e,v))
+    reverse[e].append((s,v))
+    indegree[e] += 1
+
+startCity, endCity = map(int, input().split())
 
 queue = deque()
-
-for i in range(1,n+1):
-    if indegree[i] == 0:
-        queue.append(i)
-
-result = [0]*(n+1)
+queue.append(startCity)
 
 while queue:
     now = queue.popleft()
     for next in arr[now]:
-        indegree[next] -= 1
-        result[next] = max(result[next], result[now]+selfBuild[now])
-        if indegree[next] == 0:
-            queue.append(next)
+        indegree[next[0]] -= 1
+        result[next[0]] = max(result[next[0]], result[now]+ next[1])
+        if indegree[next[0]] == 0:
+            queue.append(next[0])
 
-for i in range(1,n+1):
-    print(result[i]+selfBuild[i])
+queue.clear()
+queue.append(endCity)
+visited[endCity] = True
+
+while queue:
+    now = queue.popleft()
+    for next in reverse[now]:
+        if result[next[0]] + next[1] == result[now]:
+            count += 1
+            if not visited[next[0]]:
+                visited[next[0]] = True
+                queue.append(next[0])
+
+print(result[endCity])
+print(count)
+
 
 
 
