7151d2a672b0b7f720662dbf75e426531a047f95
this-is-this-is <dlrjs529@gmail.com>
2024-08-16 23:33:55 +0900

    #1753 G4 최단경로 / 그래프(다익스트라)

--- solved.py
+++ solved.py
@@ -1,51 +1,37 @@
 import sys
 input = sys.stdin.readline
-from collections import deque
+from queue import PriorityQueue
 
-n = int(input())
-m = int(input())
-arr = [[]for _ in range(n+1)]
-reverse = [[]for _ in range(n+1)]
-indegree = [0] *(n+1)
-result = [0]*(n+1)
-count = 0
-visited = [False]*(n+1)
+V,E = map(int, input().split())
+k = int(input())
+distance = [sys.maxsize]*(V+1)
+visited = [False]*(V+1)
+arr = [[]for _ in range(V+1)]
+q = PriorityQueue()
 
-for i in range(m):
-    s,e,v = map(int, input().split())
-    arr[s].append((e,v))
-    reverse[e].append((s,v))
-    indegree[e] += 1
+for _ in range(E):
+    u,v,w = map(int, input().split())
+    arr[u].append((v,w))
 
-startCity, endCity = map(int, input().split())
+q.put((0,k))
+distance[k] = 0
 
-queue = deque()
-queue.append(startCity)
+while q.qsize() > 0:
+    now = q.get()
+    nowV = now[1]
+    if visited[nowV]:
+        continue
+    visited[nowV] = True
+    for tmp in arr[nowV]:
+        next = tmp[0]
+        value = tmp[1]
+        if distance[next] > distance[nowV] + value:
+            distance[next] = distance[nowV] + value
+            q.put((distance[next],next))
 
-while queue:
-    now = queue.popleft()
-    for next in arr[now]:
-        indegree[next[0]] -= 1
-        result[next[0]] = max(result[next[0]], result[now]+ next[1])
-        if indegree[next[0]] == 0:
-            queue.append(next[0])
-
-queue.clear()
-queue.append(endCity)
-visited[endCity] = True
-
-while queue:
-    now = queue.popleft()
-    for next in reverse[now]:
-        if result[next[0]] + next[1] == result[now]:
-            count += 1
-            if not visited[next[0]]:
-                visited[next[0]] = True
-                queue.append(next[0])
-
-print(result[endCity])
-print(count)
-
-
-
+for i in range(1, V+1):
+    if visited[i]:
+        print(distance[i])
+    else:
+        print("INF")
 
