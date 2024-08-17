8c4b141120ecaf9cfa5727f41d45c95f3c8afc7f
this-is-this-is <dlrjs529@gmail.com>
2024-08-17 17:40:49 +0900

    #7576 G5 토마토 / BFS

--- solved.py
+++ solved.py
@@ -2,42 +2,38 @@ import sys
 from collections import deque
 input = sys.stdin.readline
 
-m, n, h = map(int, input().split())
+m, n = map(int, input().split())
 
-box = [[list(map(int, input().split())) for _ in range(n)] for _ in range(h)]
+box= [list(map(int, input().split())) for _ in range(n)]
 day = 0
-dxyz = [(0, -1, 0), (0, 1, 0), (0, 0, -1), (0, 0, 1), (1, 0, 0), (-1, 0, 0)]
-queue = deque()
+dxy = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+queue = deque() 
 
-for z in range(h):
-    for y in range(n):
-        for x in range(m):
-            if box[z][y][x] == 1:
-                queue.append([z ,y, x])
+for y in range(n):
+    for x in range(m):
+        if box[y][x] == 1:
+            queue.append([y, x])
 
 while queue:
-    z, y, x = queue.popleft()
-    for i in range(6):
-        dz, dy, dx = dxyz[i]
-        if 0 <= dx + x < m and 0 <= dy + y < n and 0 <= dz + z < h:
-            if box[z + dz][y + dy][x + dx] == 0:
-                box[z + dz][y + dy][x + dx] += box[z][y][x] + 1
-                queue.append([z + dz,y + dy, x + dx])
+    y, x = queue.popleft()
+    for i in range(4):
+        dy, dx = dxy[i]
+        if 0 <= dx + x < m and 0 <= dy + y < n:
+            if box[y + dy][x + dx] == 0:
+                box[y + dy][x + dx] += box[y][x] + 1
+                queue.append([y + dy, x + dx])
 
 isP = True
 
-for z in range(h):
+for y in range(n):
     if not isP:
         break
-    for y in range(n):
-        if not isP:
+    for x in range(m):
+        if box[y][x] == 0:
+            isP = False
             break
-        for x in range(m):
-            if box[z][y][x] == 0:
-                isP = False
-                break
-            if box[z][y][x] > day:
-                day = box[z][y][x]
+        if box[y][x] > day:
+            day = box[y][x]
 
 if isP == 0:
     print(-1)
