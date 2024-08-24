293c972e2c9cda265dda580bad7196aaf0adbd3d
this-is-this-is <dlrjs529@gmail.com>
2024-08-24 11:02:04 +0900

    #16928 G5 뱀과사다리게임 / BFS

--- solved.py
+++ solved.py
@@ -1,38 +1,43 @@
 import sys
 input = sys.stdin.readline
-from queue import PriorityQueue
-
-def find(a):
-    if a == parent[a]:
-        return a
-    else:
-        parent[a] = find(parent[a])
-        return parent[a]
-
-def union(a,b):
-    a = find(a)
-    b = find(b)
-    if a != b:
-        parent[b] = a
+from collections import deque
 
 n,m = map(int, input().split())
-pq = PriorityQueue()
-parent = [0]*(n+1)
-for i in range(n+1):
-    parent[i] = i
+
+arr = [[]for _ in range(n+1)]
+ladder = {}
+snake = {}
+for i in range(n):
+    s,e = map(int, input().split())
+    ladder[s] = e
 
 for i in range(m):
-    s,e,w = map(int, input().split())
-    pq.put((w,s,e))
+    s,e = map(int, input().split())
+    snake[s] = e
+visited = [0 for _ in range(101)]
 
-useEdge = 0
-result = 0
+q = deque()
 
-while useEdge < n - 1:
-    w,s,e = pq.get()
-    if find(s) != find(e):
-        union(s,e)
-        result+=w
-        useEdge+=1
+def BFS(v, count):
+    q.append((v,count))
+    visited[v] = True
+    while q:
+        now, cnt = q.popleft()
+        for i in range(1,7):
+            if now + i <= 100:
+                if now + i  == 100:
+                    print(cnt + 1)
+                    exit(0)
+                if not visited[now+i] and now+i in ladder:
+                    q.append((ladder[now + i],cnt+1))
+                    visited[now+i] = True
+                    visited[ladder[now+i]] = True
+                elif not visited[now+i] and now+i in snake:
+                    q.append((snake[now + i],cnt+1))
+                    visited[now+i] = True
+                    visited[snake[now+i]] = True
+                elif not visited[now+i]:
+                    q.append((now + i,cnt+1))
+                    visited[now+i] = True
+BFS(1, 0)
 
-print(result)
